8cf37f2baa989a7b985f3692c56c8c469ea8971c
Pablo Sanchez <psanchez@plus54.com>
2021-04-21 22:07:00 -0300

    Practica de clase MVC, con binging automatico

---
+++ Clase3PracticaMVC/Controllers/JugadoresController.cs
@@ -0,0 +1,88 @@
+﻿using Entidades;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clase3PracticaMVC.Controllers
+{
+    public class JugadoresController : Controller
+    {
+        public static List<Entidades.Jugador> Jugadores { get; set; }
+
+        public JugadoresController()
+        {
+            if (Jugadores == null)
+            {
+                Jugadores = new List<Jugador>();
+            }
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CrearUsandoRequest()
+        {
+            string nombre = Request.Form["Nombre"].ToString();
+            string apellido = Request.Form["Apellido"].ToString();
+            string club = Request.Form["Club"].ToString();
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Crear(string nombre, string apellido, string club)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Crear(Jugador jug)
+        {
+            string nombre = jug.Nombre;
+            string apellido = jug.Apellido;
+            string club = jug.Club;
+            return View();
+        }
+
+        public IActionResult VerDetalle()
+        {
+            Jugador j = new Jugador();
+            j.Nombre = "Pablo";
+            j.Apellido = "Aymar";
+            j.Club = "Valencia";
+
+            return View(j);
+        }
+
+        public IActionResult SinClub()
+        {
+            List<Jugador> jugSinClub = new List<Jugador>();
+            Jugador j = new Jugador();
+            j.Nombre = "Pablo";
+            j.Apellido = "Aymar";
+
+            jugSinClub.Add(j);
+            jugSinClub.Add(j);
+            jugSinClub.Add(j);
+
+            return View(jugSinClub);
+        }
+
+
+        [HttpPost]
+        public IActionResult Cargar(Jugador jug)
+        {
+            //TODO: agregar a la lista
+            return View();
+        }
+
+        public IActionResult JugadoresCargados()
+        {
+           //TODO: retornar lista (deberian tambien crear la vista)
+            return View();
+        }
+    }
+}
---
+++ Entidades/Jugador.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Entidades
+{
+    public class Jugador
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Club { get; set; }
+    }
+}
